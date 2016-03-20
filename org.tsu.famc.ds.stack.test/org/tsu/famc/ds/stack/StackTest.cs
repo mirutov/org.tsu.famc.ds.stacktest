2e7942ccaf43c17d6bf0ae523ed5081fa8db3af4
Алексей Мирютов <a.mirutov@hotmail.com>
2016-03-20 12:23:49 +0600

    Тест для стека

---
+++ org.tsu.famc.ds.stack.test/org/tsu/famc/ds/stack/StackTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using stack;
+
+namespace org.tsu.famc.ds.stack
+{
+    class StackTest
+    {
+        static void Main(string[] args)
+        {
+            Stack s = new Stack();
+            try
+            {
+                for (int i = 0; i < 1000; i++)
+                {
+                    s.Push(i);
+                }
+                Stack s1 = new Stack(s);
+                while (true)
+                {
+                    int tmp = (int)s1.Top();
+                    if (tmp % 100 == 0)
+                        Console.WriteLine(tmp);
+                    if (tmp != (int)s1.Pop())
+                        throw new Exception("Test failed");
+                }
+            }
+            catch (EStackEmpty e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
